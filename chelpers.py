563292f52206b72081dba062c2f8442b26599162
Exceed IO Inc <arthurlee087@gmail.com>
2022-08-13 21:59:52 -0500

    Add files via upload

---
+++ chelpers.py
@@ -0,0 +1,32 @@
+import FbLiteral
+import C Literal
+import F Literal
+import PhrygianLiteral
+import FSharpLiteral
+import cHelperPortablizer
+import Harmonic MinorLiteral
+import Three FourLiteral
+import Four EightLiteral
+import ASharpLiteral
+import LocrianLiteral
+import Natural MinorLiteral
+import CSharpLiteral
+import A Literal
+import Five FourLiteral
+import Four TwoLiteral
+import DSharpLiteral
+import D Literal
+import DorianLiteral
+import Two FourLiteral
+import BbLiteral
+import G Literal
+import chelpers
+import Two TwoLiteral
+import GbLiteral
+import B Literal
+import E Literal
+import Four FourLiteral
+import Melodic MinorLiteral
+import Ab Literal
+import LydianLiteral
+import EbLiteral
